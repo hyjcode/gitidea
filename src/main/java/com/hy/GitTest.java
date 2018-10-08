5487d97f9e81dea7cbc6bd64a0374f09c563fdcc
dxj0607 <dxj0607@163.com>
2018-10-08 09:30:30 +0800

    左边更新

--- src/main/java/com/hy/GitTest.java
+++ src/main/java/com/hy/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("再次上传");
+        System.out.println("左边更新");
     }
 }
